4c1096a5425f83a8dbf2ae18e37a214714bd2b98
chao <754133562@qq.com>
2020-01-06 17:54:38 +0800

    Initial Commit

---
+++ MAXHealthAPP/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MAXHealthAPP
+//
+//  Created by 李逸超 on 2020/1/6.
+//  Copyright © 2020 李逸超. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MAXHealthAPPTests/MAXHealthAPPTests.swift
@@ -0,0 +1,34 @@
+//
+//  MAXHealthAPPTests.swift
+//  MAXHealthAPPTests
+//
+//  Created by 李逸超 on 2020/1/6.
+//  Copyright © 2020 李逸超. All rights reserved.
+//
+
+import XCTest
+@testable import MAXHealthAPP
+
+class MAXHealthAPPTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
